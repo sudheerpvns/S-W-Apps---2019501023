6155e65435dab12491dd5e34bb9a00d7cb6db05a
sudheer panangipalli <pvnssudheer@msitprogram.net>
2020-05-07 19:18:03 +0530

    updated with no duplicate tasks, clearing input field if clicked once strike-through twice no striking

--- Practice/todo.js
+++ Practice/todo.js
@@ -1,16 +1,23 @@
-function addTask() {
+function addTask(task) {
   console.log('HERE')
-  let task = document.getElementById('input').value
+  
   console.log(task)
   let point = document.getElementById('list')
   let li = document.createElement('li')
   let but = document.createElement("button")
+  but.setAttribute("class", 'button')
   but.innerHTML = "Delete"
   
   if (task.length > 0) {
-    li.appendChild(document.createTextNode('-- '+task+"   "))
-    li.append(but)
-    point.appendChild(li)
+    if (check(task)) {
+      li.appendChild(document.createTextNode('---   '+task+"   "))
+      li.append(but)
+      point.appendChild(li)
+      task.value ="reset"
+    }
+    else{
+      alert("Task Exists")
+    }
   }
 
   else{
@@ -20,16 +27,43 @@ function addTask() {
   li.onclick = strike
   
 }
+
+function check(item) {
+  let tag = document.querySelectorAll('li')
+  console.log(tag)
+  if (tag.length === 0) return true
+  console.log(tag[0].textContent)
+  for (let i = 0; i < tag.length;i++) {
+    let pre = tag[i].textContent.split(" ")
+    console.log(pre)
+    console.log(pre[3])
+    console.log(item)
+    if (item === pre[3]) 
+    return false
+  }
+  return true
+}
+
 document.getElementById('submit').addEventListener("click",function() {
-  addTask()
+  let task = document.getElementById('input').value
+  let blank = document.getElementById('input')
+  blank.value = ""
+  addTask(task)
 })
 
 function remove(e) {
   e.target.parentElement.remove(e.target)
 }
 
-
+let flag = false
 function strike(e) {
-  this.style.textDecoration="line-through"
+  if (!flag) {
+    flag = true
+    this.style.textDecoration="line-through"
+  }
+  else{
+    this.style.textDecoration="none"
+    flag = false
+  }
 }
 
